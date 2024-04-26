85d86fb6d59787a68319cf0df18888b3177ffa7d
Rich Lott / Artful Robot <code.commits@artfulrobot.uk>
2024-04-26 15:44:19 +0100

    Remove debugging code

--- lua/bufjump.lua
+++ lua/bufjump.lua
@@ -1,31 +1,3 @@
-local debug = function(s)
-  local f = assert(io.open('/home/rich/lua.log', 'a'));
-  f:write(s .. "\n")
-  f:close()
-end
-
-local dumpjumps = function()
-  local jumplistAndPos = vim.fn.getjumplist()
-  local jumplist = jumplistAndPos[1]
-  local lastUsedJumpPos = jumplistAndPos[2] + 1
-  if #jumplist == 0 then
-    debug("(no jumplist)")
-    return
-  end
-
-  local curBufNum = vim.fn.bufnr()
-  debug("curBufNum " .. curBufNum)
-  local i
-  for i, item in pairs(jumplist) do
-    local hl = ''
-    if curBufNum == item.bufnr then hl = hl .. 'this ' else hl = hl .. '     ' end
-    if i == lastUsedJumpPos then hl = hl .. '*' else hl = hl .. ' ' end
-    debug(hl ..
-      i ..
-      ' buf:' .. item.bufnr .. ' L' .. (item.lnum or '(no line number)') .. ': ' .. (item.filename or '(no filename)'))
-  end
-  debug("TEST: lastUsedJumpPos: " .. tostring(lastUsedJumpPos) .. " L" .. jumplist[lastUsedJumpPos].lnum)
-end
 local on_success = nil
 local jumpbackward = function(num)
   vim.cmd([[execute "normal! ]] .. tostring(num) .. [[\<c-o>"]])
@@ -61,13 +33,10 @@ local backward = function()
 end
 
 local backwardSameBuf = function()
-  debug("backwardSameBuf called")
-  -- dumpjumps()
   local jumplistAndPos = vim.fn.getjumplist()
 
   local jumplist = jumplistAndPos[1]
   if #jumplist == 0 then
-    debug("no jump list, ignoring backwardSameBufKey")
     return
   end
   local lastUsedJumpPos = jumplistAndPos[2] + 1
@@ -79,22 +48,15 @@ local backwardSameBuf = function()
     j = j - 1
     if j > 0 and (curBufNum == jumplist[j].bufnr and vim.api.nvim_buf_is_valid(jumplist[j].bufnr)) then
       foundJump = true
-      debug("↑ " .. j .. " buf:" .. jumplist[j].bufnr .. " MATCH")
-    elseif j > 0 then
-      debug("↑ " .. j .. " buf:" .. jumplist[j].bufnr .. " no match")
-    else
-      debug("↑ hit end");
     end
   until j == 0 or foundJump
 
   if foundJump then
-    debug("CHOSE " .. j)
     jumpbackward(lastUsedJumpPos - j)
     if on_success then
       on_success()
     end
   end
-  debug("done backwardSameBuf")
 end
 
 local forward = function()
@@ -132,7 +94,6 @@ local forwardSameBuf = function()
   local jumplistAndPos = vim.fn.getjumplist()
   local jumplist = jumplistAndPos[1]
   if #jumplist == 0 then
-    debug("no jump list, ignoring forwardSameBufKey")
     return
   end
   local lastUsedJumpPos = jumplistAndPos[2] + 1
@@ -144,22 +105,14 @@ local forwardSameBuf = function()
     j = j + 1
     if j <= #jumplist and curBufNum == jumplist[j].bufnr and vim.api.nvim_buf_is_valid(jumplist[j].bufnr) then
       foundJump = true
-      debug("↓ " .. j .. " buf:" .. jumplist[j].bufnr .. " MATCHES")
-    elseif j <= #jumplist then
-      debug("↓ " .. j .. " buf:" .. jumplist[j].bufnr .. " no matches")
-    else
-      debug("↓ " .. j .. " hit end")
     end
   until j > #jumplist or foundJump
 
   if foundJump then
-    debug("CHOSE " .. j .. ' from pos ' .. lastUsedJumpPos)
     jumpforward(j - lastUsedJumpPos)
     if on_success then
       on_success()
     end
-  else
-    debug("ends no foundJump")
   end
 end
 
