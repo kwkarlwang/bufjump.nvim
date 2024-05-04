85d1d8d8bbf0a3012c78068a5ad47384a7da38c7
Rich Lott <artfulrobot@users.noreply.github.com>
2024-05-04 22:54:45 +0100

    feat!: same buf jump. `forward` to `forward_key`, `backward` to `backward_key`
    * initial attempt at same buffer jump
    
    * Add forward/backward in same buffer
    
    * Remove debugging code
    
    * update README
    
    * update README to make the example config match the suggested keymaps
    
    * remove left-over debugging call
    
    * Standardise on snake_case
    
    * Modernise keymap setting for nvim 0.7+
    
    Modernise keymap setting for nvim 0.7+
    
    Further simplify keybinds
    
    Fix backwards keymap default

--- README.md
+++ README.md
@@ -4,9 +4,11 @@ https://user-images.githubusercontent.com/38927155/132891665-88f70573-c1d8-462d-
 
 Have you ever had to temporarily go to another file, perhaps previewing some changes with lsp go to refrenece, fiddle a bit, and have to jump back to the main file that you were working with? Instead of aimlessly smashing `CTRL-o` and `CTRL-i`, bufjump.nvim allows you to jump to previous or next buffer in the vim native jumplist with one single command.
 
+Or, have you ever wanted to browse your jumplist *within* the current buffer, *not* jumping outside that buffer? Read on.
+
 ## Prerequistes
 
-- Neovim 0.5 or higher
+- Neovim 0.7 or higher
 
 ## Installing
 
@@ -29,7 +31,13 @@ use {
 
 ## Configuration
 
-bufjump.nvim provides three options, `forward`, `backward` and `on_success`. `forward` and `backward` are the keymappings to jump to the next and previous buffer in the jumplist respectively. The default keymappings for `forward` and `backward` are `CTRL-n` and `CTRL-p` respectively.
+bufjump.nvim provides the following configuration options:
+
+- `forward_key`, `backward_key` the keymappings to jump to the next and previous 
+  buffer in the jumplist respectively. The default keymappings for `forward` and `backward` are `CTRL-n` and `CTRL-p` respectively. You can deactivate these keybindings by passing `false`.
+
+- `forward_same_bufKey`, `backward_same_bufKey` provide keymappings to jump the the 
+  next and previous jumplist positions within the same buffer. These have no default keybindings. Suggestion: `<M-i>` and `<M-o>`.
 
 Default configuration:
 
@@ -38,8 +46,8 @@ use({
     "kwkarlwang/bufjump.nvim",
     config = function()
         require("bufjump").setup({
-            forward = "<C-n>",
-            backward = "<C-p>",
+            forward_key = "<C-n>",
+            backward_key = "<C-p>",
             on_success = nil
         })
     end,
@@ -47,12 +55,14 @@ use({
 
 ```
 
-You can also bind the function `forward` and `backward` as followed
+You can also bind the function `forward`, `backward` `forward_same_buf`, `backward_same_buf` as followed
 
 ```
 local opts = { silent=true, noremap=true }
 vim.api.nvim_set_keymap("n", "<M-o>", ":lua require('bufjump').backward()<cr>", opts)
 vim.api.nvim_set_keymap("n", "<M-i>", ":lua require('bufjump').forward()<cr>", opts)
+vim.api.nvim_set_keymap("n", "<M-o>", ":lua require('bufjump').backward_same_buf()<cr>", opts)
+vim.api.nvim_set_keymap("n", "<M-i>", ":lua require('bufjump').forward_same_buf()<cr>", opts)
 ```
 
 ### on_success
--- lua/bufjump.lua
+++ lua/bufjump.lua
@@ -1,76 +1,144 @@
 local on_success = nil
+
 local jumpbackward = function(num)
-	vim.cmd([[execute "normal! ]] .. tostring(num) .. [[\<c-o>"]])
+  vim.cmd([[execute "normal! ]] .. tostring(num) .. [[\<c-o>"]])
 end
 
 local jumpforward = function(num)
-	vim.cmd([[execute "normal! ]] .. tostring(num) .. [[\<c-i>"]])
+  vim.cmd([[execute "normal! ]] .. tostring(num) .. [[\<c-i>"]])
 end
+
 local backward = function()
-	local getjumplist = vim.fn.getjumplist()
-	local jumplist = getjumplist[1]
-	if #jumplist == 0 then
-		return
-	end
+  local getjumplist = vim.fn.getjumplist()
+  local jumplist = getjumplist[1]
+  if #jumplist == 0 then
+    return
+  end
 
-	-- plus one because of one index
-	local i = getjumplist[2] + 1
-	local j = i
-	local curBufNum = vim.fn.bufnr()
-	local targetBufNum = curBufNum
+  -- plus one because of one index
+  local i = getjumplist[2] + 1
+  local j = i
+  local curBufNum = vim.fn.bufnr()
+  local targetBufNum = curBufNum
 
-	while j > 1 and (curBufNum == targetBufNum or not vim.api.nvim_buf_is_valid(targetBufNum)) do
-		j = j - 1
-		targetBufNum = jumplist[j].bufnr
-	end
-	if targetBufNum ~= curBufNum and vim.api.nvim_buf_is_valid(targetBufNum) then
-		jumpbackward(i - j)
-		if on_success then
-			on_success()
-		end
-	end
+  while j > 1 and (curBufNum == targetBufNum or not vim.api.nvim_buf_is_valid(targetBufNum)) do
+    j = j - 1
+    targetBufNum = jumplist[j].bufnr
+  end
+  if targetBufNum ~= curBufNum and vim.api.nvim_buf_is_valid(targetBufNum) then
+    jumpbackward(i - j)
+    if on_success then
+      on_success()
+    end
+  end
+end
+
+local backward_same_buf = function()
+  local jumplistAndPos = vim.fn.getjumplist()
+
+  local jumplist = jumplistAndPos[1]
+  if #jumplist == 0 then
+    return
+  end
+  local lastUsedJumpPos = jumplistAndPos[2] + 1
+  local curBufNum = vim.fn.bufnr()
+
+  local j = lastUsedJumpPos
+  local foundJump = false
+  repeat
+    j = j - 1
+    if j > 0 and (curBufNum == jumplist[j].bufnr and vim.api.nvim_buf_is_valid(jumplist[j].bufnr)) then
+      foundJump = true
+    end
+  until j == 0 or foundJump
+
+  if foundJump then
+    jumpbackward(lastUsedJumpPos - j)
+    if on_success then
+      on_success()
+    end
+  end
 end
 
 local forward = function()
-	local getjumplist = vim.fn.getjumplist()
-	local jumplist = getjumplist[1]
-	if #jumplist == 0 then
-		return
-	end
+  local getjumplist = vim.fn.getjumplist()
+  local jumplist = getjumplist[1]
+  if #jumplist == 0 then
+    return
+  end
 
-	local i = getjumplist[2] + 1
-	local j = i
-	local curBufNum = vim.fn.bufnr()
-	local targetBufNum = curBufNum
+  local i = getjumplist[2] + 1
+  local j = i
+  local curBufNum = vim.fn.bufnr()
+  local targetBufNum = curBufNum
 
-	-- find the next different buffer
-	while j < #jumplist and (curBufNum == targetBufNum or vim.api.nvim_buf_is_valid(targetBufNum) == false) do
-		j = j + 1
-		targetBufNum = jumplist[j].bufnr
-	end
-	while j + 1 <= #jumplist and jumplist[j + 1].bufnr == targetBufNum and vim.api.nvim_buf_is_valid(targetBufNum) do
-		j = j + 1
-	end
-	if j <= #jumplist and targetBufNum ~= curBufNum and vim.api.nvim_buf_is_valid(targetBufNum) then
-		jumpforward(j - i)
+  -- find the next different buffer
+  while j < #jumplist and (curBufNum == targetBufNum or vim.api.nvim_buf_is_valid(targetBufNum) == false) do
+    j = j + 1
+    targetBufNum = jumplist[j].bufnr
+  end
+  while j + 1 <= #jumplist and jumplist[j + 1].bufnr == targetBufNum and vim.api.nvim_buf_is_valid(targetBufNum) do
+    j = j + 1
+  end
+  if j <= #jumplist and targetBufNum ~= curBufNum and vim.api.nvim_buf_is_valid(targetBufNum) then
+    jumpforward(j - i)
 
-		if on_success then
-			on_success()
-		end
-	end
+    if on_success then
+      on_success()
+    end
+  end
 end
+
+
+local forward_same_buf = function()
+  local jumplistAndPos = vim.fn.getjumplist()
+  local jumplist = jumplistAndPos[1]
+  if #jumplist == 0 then
+    return
+  end
+  local lastUsedJumpPos = jumplistAndPos[2] + 1
+  local curBufNum = vim.fn.bufnr()
+
+  local j = lastUsedJumpPos
+  local foundJump = false
+  repeat
+    j = j + 1
+    if j <= #jumplist and curBufNum == jumplist[j].bufnr and vim.api.nvim_buf_is_valid(jumplist[j].bufnr) then
+      foundJump = true
+    end
+  until j > #jumplist or foundJump
+
+  if foundJump then
+    jumpforward(j - lastUsedJumpPos)
+    if on_success then
+      on_success()
+    end
+  end
+end
+
 local setup = function(cfg)
-	local opts = { silent = true, noremap = true }
-	cfg = cfg or {}
-	local forwardkey = cfg.forward or "<C-n>"
-	local backwardkey = cfg.backward or "<C-p>"
-	on_success = cfg.on_success or nil
-	vim.api.nvim_set_keymap("n", backwardkey, ":lua require('bufjump').backward()<cr>", opts)
-	vim.api.nvim_set_keymap("n", forwardkey, ":lua require('bufjump').forward()<cr>", opts)
+  local bufjump = require('bufjump')
+  cfg = cfg or {}
+  if cfg.forward_key ~= false then
+    local forward_key = cfg.forward_key or "<C-n>"
+    vim.keymap.set("n", forward_key, bufjump.forward)
+  end
+  if cfg.backward_key ~= false then
+    local backward_key = cfg.backward_key or "<C-p>"
+    vim.keymap.set("n", backward_key, bufjump.backward)
+  end
+  if cfg.forward_same_buf_key then
+    vim.keymap.set("n", cfg.forward_same_buf_key, bufjump.forward_same_buf)
+  end
+  if cfg.backward_same_buf_key then
+    vim.keymap.set("n", cfg.backward_same_buf_key, bufjump.backward_same_buf)
+  end
 end
 
 return {
-	backward = backward,
-	forward = forward,
-	setup = setup,
+  backward = backward,
+  forward = forward,
+  backward_same_buf = backward_same_buf,
+  forward_same_buf = forward_same_buf,
+  setup = setup,
 }
